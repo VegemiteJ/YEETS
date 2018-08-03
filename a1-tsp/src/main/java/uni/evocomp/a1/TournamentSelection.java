bda643ad70edc4b6afddf14f5bcbd0249e1eda55
joshloh <joshuafloh@gmail.com>
2018-08-03 13:30:42 +0930

    #24 modified tournamentselect to work with evaluate class

--- a1-tsp/src/main/java/uni/evocomp/a1/TournamentSelection.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/TournamentSelection.java
@@ -6,13 +6,16 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Set;
-import uni.evocomp.util.Util;
 
 public class TournamentSelection implements SelectSurvivors {
   // TODO: Decide whether to store these here or pass them directly to the function
-
+  private Evaluate evaluate;
   private Integer tournamentSize;
   private Double p;
+
+  TournamentSelection() {
+    this.evaluate = new EvaluateEuclid();
+  }
 
   @Override
   public Population selectSurvivors(Population population, TSPProblem problem, Random rand) {
@@ -32,12 +35,13 @@ public class TournamentSelection implements SelectSurvivors {
     }
     // Sort the list based on fitness
     // https://stackoverflow.com/questions/49122512/sorting-an-arraylist-based-on-the-result-of-a-method
-    tournamentList.sort((o1, o2) -> Util.calculateFitness(o1, problem) - Util.calculateFitness((o2, problem)));
+    tournamentList
+        .sort((o1, o2) -> (int) (evaluate.evaluate(problem, o1) - evaluate.evaluate(problem, o2)));
 
     // Choose the best individual with probability p, 2nd with p(1-p), 3rd with p(1-p)^2 etc.
     int i = 0;
     for (Individual individual : tournamentList) {
-      if (rand.nextFloat() < (p*Math.pow((1-p), i))) {
+      if (rand.nextFloat() < (p * Math.pow((1 - p), i))) {
         survivorSet.add(individual);
       }
       i++;
