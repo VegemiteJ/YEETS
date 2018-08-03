3849596b57c7c3c9adbd115aa333c5370905f097
Namdrib <dphosavanhau+github@gmail.com>
2018-08-03 18:50:51 +0930

    Issue #43, tests for Util classes. Test case for each function, Util as a suite

---
+++ a1-tsp/src/test/java/uni/evocomp/a1/EuclideanDistance2DIntegerPairTest.java
@@ -0,0 +1,64 @@
+package uni.evocomp.a1;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import uni.evocomp.util.IntegerPair;
+import uni.evocomp.util.Util;
+
+public class EuclideanDistance2DIntegerPairTest {
+
+  IntegerPair nullIntegerPair;
+  IntegerPair realIntegerPair;
+  double delta;
+
+  @Before
+  public void setUp() throws Exception {
+    realIntegerPair = new IntegerPair(0, 0);
+    delta = 0.001;
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullI() {
+    Util.euclideanDistance2D(nullIntegerPair, realIntegerPair);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullJ() {
+    Util.euclideanDistance2D(realIntegerPair, nullIntegerPair);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void testNullBoth() {
+    Util.euclideanDistance2D(nullIntegerPair, nullIntegerPair);
+  }
+
+  @Test
+  public void testSameIntegerPair() {
+    assertEquals(1.0, Util.euclideanDistance2D(realIntegerPair, realIntegerPair), delta);
+  }
+
+  @Test
+  public void testDifferentIntegerPairs() {
+    assertEquals(6.0, Util.euclideanDistance2D(new IntegerPair(1, 1), new IntegerPair(5, 5)),
+        delta);
+  }
+
+  @Test
+  public void testDifferentIntegerPairsReverse() {
+    assertEquals(6.0, Util.euclideanDistance2D(new IntegerPair(5, 5), new IntegerPair(1, 1)),
+        delta);
+  }
+
+  @Test
+  public void testBothNegative() {
+    assertEquals(6.0, Util.euclideanDistance2D(new IntegerPair(-1, -1), new IntegerPair(-5, -5)),
+        delta);
+  }
+
+  @Test
+  public void testOneNegative() {
+    assertEquals(6.0, Util.euclideanDistance2D(new IntegerPair(-1, -1), new IntegerPair(3, 3)),
+        delta);
+  }
+}
--- a1-tsp/src/test/java/uni/evocomp/a1/MutateTests.java
+++ a1-tsp/src/test/java/uni/evocomp/a1/MutateTests.java
@@ -12,5 +12,5 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({InsertTest.class, InvertTest.class, SwapTest.class})
-public class MutateTest {
+public class MutateTests {
 }
