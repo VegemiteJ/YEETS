b433a41bd4b06ed7dcca9ff6750709a54aff5795
Namdrib <dphosavanhau+github@gmail.com>
2018-08-06 00:23:24 +0930

    Issue #57, fixed my typo in EvaluateEuclidTest.testFileAgainstBest()

--- a1-tsp/src/main/java/uni/evocomp/a1/EvaluateEuclid.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/EvaluateEuclid.java
@@ -10,8 +10,9 @@ public class EvaluateEuclid implements Evaluate {
     List<Integer> genotype = individual.getGenotype();
     for (int i = 0; i < genotype.size() - 1; i++) {
       // TODO: Loading tour file has the last element as -1, should change in TSPIO
-      if (genotype.get(i + 1) == -1)
+      if (genotype.get(i + 1) == -1) {
         break;
+      }
       cost += weights.get(genotype.get(i) - 1).get(genotype.get(i + 1) - 1);
     }
     return cost;
