5f4aa2b6ebb57fb79c8a29ddb2786a2a9516b648
Jack Baxter <baxter.jack@outlook.com>
2018-08-05 16:23:50 +0930

    quick change to add terminating condition

--- a1-tsp/src/main/java/uni/evocomp/a1/LocalSearch.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/LocalSearch.java
@@ -11,47 +11,57 @@ import java.util.Random;
 
 public class LocalSearch {
 
-    public static void main(String[] args) {
-        new LocalSearch();
+  public static void main(String[] args) {
+    new LocalSearch();
+  }
+
+  LocalSearch() {
+    // Load Problem
+    TSPProblem problem = new TSPProblem();
+    TSPIO io = new TSPIO();
+    try (Reader r = new FileReader("tests/eil76.tsp")) {
+      problem = io.read(r);
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
-    LocalSearch() {
-        //Load Problem
-        TSPProblem problem = new TSPProblem();
-        TSPIO io = new TSPIO();
-        try (Reader r = new FileReader("tests/eil76.tsp")) {
-            problem = io.read(r);
-        } catch (IOException e) {
-            e.printStackTrace();
+    // Evaluate function
+    Evaluate evaluate = new EvaluateEuclid();
+
+    // Initial solution
+    Individual currentBestIndividual = new Individual(problem.getSize());
+    double currentBestCost = evaluate.evaluate(problem, currentBestIndividual);
+
+    // Mutate function
+    Mutate N = new Jump();
+    Random rand = new Random();
+
+    //            Rand jump
+    //            int first = rand.nextInt(problem.getSize()-1);
+    //            int second = rand.nextInt(problem.getSize()-1);
+
+    int totalIterations = 0;
+
+    boolean madeChange = false;
+    while (!madeChange) {
+      for (int idxA=0; idxA<problem.getSize()-1; idxA++) {
+        for (int idxB=0; idxB<problem.getSize()-1; idxB++) {
+          Individual s = new Individual(currentBestIndividual);
+          N.run(s, new ArrayList<>(Arrays.asList(new IntegerPair(idxA, idxB))));
+          double cost = evaluate.evaluate(problem, s);
+          if (cost < currentBestCost) {
+            currentBestCost = cost;
+            currentBestIndividual = s;
+            System.out.println("");
+            System.out.println("New Best: " + cost);
+            madeChange = true;
+          } else {
+            System.out.print(".");
+          }
+          totalIterations++;
         }
-
-        //Evaluate function
-        Evaluate evaluate = new EvaluateEuclid();
-
-        //Initial solution
-        Individual currentBestIndividual = new Individual(problem.getSize());
-        double currentBestCost = evaluate.evaluate(problem, currentBestIndividual);
-
-        //Mutate function
-        Mutate N = new Jump();
-        Random rand = new Random();
-
-        for (int i=0; i<10000; i++) {
-            //Rand jump
-            Individual s = new Individual(currentBestIndividual);
-            int first = rand.nextInt(problem.getSize()-1);
-            int second = rand.nextInt(problem.getSize()-1);
-            N.run(s, new ArrayList<>(Arrays.asList(new IntegerPair(first, second))));
-            double cost = evaluate.evaluate(problem, s);
-
-            if(cost < currentBestCost) {
-                currentBestCost = cost;
-                currentBestIndividual = s;
-                System.out.println("");
-                System.out.println("New Best: " + cost);
-            } else {
-                System.out.print(".");
-            }
-        }
+      }
     }
+    System.out.println("\nTotal iterations was "+totalIterations );
+  }
 }
