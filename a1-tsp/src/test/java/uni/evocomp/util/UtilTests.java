4a3aa98ae130ad3c744e1fe1f08858460ad18670
joshloh <joshuafloh@gmail.com>
2018-08-06 23:09:49 +0930

    #24 Removed git merge artefact in UtilTests

--- a1-tsp/src/test/java/uni/evocomp/util/UtilTests.java
+++ a1-tsp/src/test/java/uni/evocomp/util/UtilTests.java
@@ -5,7 +5,6 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-<<<<<<< HEAD
 @SuiteClasses({ClassFromNameTest.class, EuclideanDistance2DIntegerPairTest.class,
     InBoundsTest.class})
 public class UtilTests {
