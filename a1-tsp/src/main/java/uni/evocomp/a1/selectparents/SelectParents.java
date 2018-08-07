2a33b8e64364ec039f6ca73493e58c4a29594348
joshloh <joshuafloh@gmail.com>
2018-08-07 14:09:33 +0930

    #68 finished making SelectParents interface

--- a1-tsp/src/main/java/uni/evocomp/a1/selectparents/SelectParents.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/selectparents/SelectParents.java
@@ -1,4 +1,13 @@
 package uni.evocomp.a1.selectparents;
 
+import java.util.List;
+import uni.evocomp.a1.Individual;
+import uni.evocomp.a1.Population;
+import uni.evocomp.util.Pair;
+
 /** @author joshuafloh */
-public interface SelectParents {}
+public interface SelectParents {
+  public List<Pair<Individual,Individual>> selectParents(Population population);
+}
+
+
