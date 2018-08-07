875a5a10bae7f37c6865f25792f9b871b531a454
joshloh <joshuafloh@gmail.com>
2018-08-07 16:23:42 +0930

    #73 initialised test

--- a1-tsp/src/main/java/uni/evocomp/a1/recombine/Recombine.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/recombine/Recombine.java
@@ -4,5 +4,5 @@ import uni.evocomp.a1.Individual;
 import uni.evocomp.util.Pair;
 
 public interface Recombine {
-  Pair<Individual, Individual> recombine(Individual firstParent, Individual secondParent);
+  public Pair<Individual, Individual> recombine(Individual firstParent, Individual secondParent);
 }
---
+++ a1-tsp/src/test/java/uni/evocomp/a1/recombine/RecombineDummyTest.java
@@ -0,0 +1,13 @@
+package uni.evocomp.a1.recombine;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RecombineDummyTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
