3c67507e0fa6e67b3eb8f1b71754d0ee9730b369
joshloh <joshuafloh@gmail.com>
2018-08-07 16:47:47 +0930

    #73 changed RecombineDummy to make copies instead of return the parent itself

--- a1-tsp/src/main/java/uni/evocomp/a1/recombine/RecombineDummy.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/recombine/RecombineDummy.java
@@ -5,7 +5,7 @@ import uni.evocomp.util.Pair;
 
 public class RecombineDummy implements Recombine {
   public Pair<Individual, Individual> recombine(Individual firstParent, Individual secondParent) {
-    Pair<Individual, Individual> o = new Pair<>(firstParent, secondParent);
+    Pair<Individual, Individual> o = new Pair<>(new Individual(firstParent), new Individual(secondParent));
     return o;
   }
 }
