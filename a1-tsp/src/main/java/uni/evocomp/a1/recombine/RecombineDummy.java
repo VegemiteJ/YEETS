30f14dbb07072b4a47b9b3ebdc51e90e251302da
joshloh <joshuafloh@gmail.com>
2018-08-07 16:20:47 +0930

    #73 finished recombine dummy

--- a1-tsp/src/main/java/uni/evocomp/a1/recombine/RecombineDummy.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/recombine/RecombineDummy.java
@@ -1,5 +1,11 @@
 package uni.evocomp.a1.recombine;
 
-public class RecombineDummy {
+import uni.evocomp.a1.Individual;
+import uni.evocomp.util.Pair;
 
+public class RecombineDummy implements Recombine {
+  public Pair<Individual, Individual> recombine(Individual firstParent, Individual secondParent) {
+    Pair<Individual, Individual> o = new Pair<>(firstParent, secondParent);
+    return o;
+  }
 }
