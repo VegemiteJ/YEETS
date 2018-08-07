b7ad5d29a9464cf7b1b7989bc1202594ba738911
joshloh <joshuafloh@gmail.com>
2018-08-07 14:10:31 +0930

    #70 created UniformRandom class

---
+++ a1-tsp/src/main/java/uni/evocomp/a1/selectparents/UniformRandom.java
@@ -0,0 +1,16 @@
+package uni.evocomp.a1.selectparents;
+
+import java.util.List;
+import uni.evocomp.a1.Individual;
+import uni.evocomp.a1.Population;
+import uni.evocomp.util.Pair;
+
+public class UniformRandom implements SelectParents {
+
+  @Override
+  public List<Pair<Individual, Individual>> selectParents(Population population) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
