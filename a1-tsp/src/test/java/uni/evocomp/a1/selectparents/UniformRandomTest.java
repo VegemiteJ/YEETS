d5e23415cc2ba6550dae326788e8f9d4bd18c85b
joshloh <joshuafloh@gmail.com>
2018-08-07 14:30:52 +0930

    #70 initialised test for UniformRandom

---
+++ a1-tsp/src/test/java/uni/evocomp/a1/selectparents/UniformRandomTest.java
@@ -0,0 +1,13 @@
+package uni.evocomp.a1.selectparents;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class UniformRandomTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
