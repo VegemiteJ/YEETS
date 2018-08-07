ce167e8be0c76daaad1ce34266e21cf96ea6e1ab
joshloh <joshuafloh@gmail.com>
2018-08-07 16:31:25 +0930

    #73 Finished implementing and testing RecombineDummy

--- a1-tsp/src/test/java/uni/evocomp/a1/recombine/RecombineDummyTest.java
+++ a1-tsp/src/test/java/uni/evocomp/a1/recombine/RecombineDummyTest.java
@@ -1,13 +1,27 @@
 package uni.evocomp.a1.recombine;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
+import uni.evocomp.a1.Individual;
+import uni.evocomp.util.Pair;
 
 public class RecombineDummyTest {
 
+  @Before
+  public void setUp() {
+    
+  }
+
+
   @Test
   public void test() {
-    fail("Not yet implemented");
+    Individual i0 = new Individual();
+    Individual i1 = new Individual();
+    RecombineDummy dummy = new RecombineDummy();
+    Pair<Individual,Individual> result = dummy.recombine(i0, i1);
+    assertEquals(i0, result.first);
+    assertEquals(i1, result.second);
   }
 
 }
