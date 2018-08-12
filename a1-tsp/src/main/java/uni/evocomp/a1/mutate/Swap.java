38968641672c795672610f89f57bbf9f1f9c0c29
Robert McAuley <roboprez0@gmail.com>
2018-08-12 16:24:26 +0930

    Fixed edge case in swap

--- a1-tsp/src/main/java/uni/evocomp/a1/mutate/Swap.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/mutate/Swap.java
@@ -41,6 +41,10 @@ public class Swap implements Mutate {
     }
   }
 
+  /*
+  edge case if consecutive indices are swapped
+  either i-j or j-i is doubled
+   */
   private double calculateDifferentialCost(
       TSPProblem problem, Individual individual, int i, int j) {
     List<Integer> g = individual.getGenotype();
@@ -63,10 +67,15 @@ public class Swap implements Mutate {
     differentialCost += weights.get(g.get(i_minus_1) - 1, g.get(i) - 1);
     // (i,i+1)
     differentialCost += weights.get(g.get(i) - 1, g.get(i_plus_1) - 1);
-    // (j-1,j)
-    differentialCost += weights.get(g.get(j_minus_1) - 1, g.get(j) - 1);
-    // (j,j+1)
-    differentialCost += weights.get(g.get(j) - 1, g.get(j_plus_1) - 1);
+
+    // (j-1,j) -- don't repeat if == (i,i+1)
+    if(j_minus_1 != i) {
+      differentialCost += weights.get(g.get(j_minus_1) - 1, g.get(j) - 1);
+    }
+    // (j,j+1) -- don't repeat if == (i-1,i)
+    if (j_plus_1 != i) {
+      differentialCost += weights.get(g.get(j) - 1, g.get(j_plus_1) - 1);
+    }
     return differentialCost;
   }
 }
