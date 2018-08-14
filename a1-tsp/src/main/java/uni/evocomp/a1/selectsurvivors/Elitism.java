d9a854e44d9a6121f1cb1e33636beb1b90891a74
Namdrib <dphosavanhau+github@gmail.com>
2018-08-14 23:55:34 +0930

    #25 finish class, start tests

--- a1-tsp/src/main/java/uni/evocomp/a1/selectsurvivors/Elitism.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/selectsurvivors/Elitism.java
@@ -2,8 +2,10 @@ package uni.evocomp.a1.selectsurvivors;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import uni.evocomp.a1.Individual;
 import uni.evocomp.a1.Population;
 import uni.evocomp.a1.TSPProblem;
@@ -13,6 +15,7 @@ import uni.evocomp.a1.TSPProblem;
  * fitness straight up.
  * 
  * From the lecture slides (01-Intro:47)
+ * 
  * <pre>
  * - Most EAs use fixed population size so need a way of going from (parents + offspring) to next generation
  * - Often deterministic
@@ -38,33 +41,42 @@ public class Elitism implements SelectSurvivors {
 
   /**
    * Don't need to use <code>rand</code>
-   * Assume the first half of the population is the parents, next half is children.
-   * Therefore only want to steal some percentage of the first half
+   * 
+   * Assume the first half of the population is the parents, next half is children. Therefore only
+   * want to steal some percentage of the first half
+   * 
    * As a backing algorithm, call tournament select on the remaining Individuals
    */
   @Override
   public Population selectSurvivors(Population population, TSPProblem problem, Random rand) {
-    List<Individual> sortedPopulation = new ArrayList<>();
-    
-    // Really hope here that the population is ordered by insertion order
-    int i = 0;
-    for (Individual individual : population.getPopulation()) {
-      if (i > population.getSize()) {
-        break;
-      }
-      sortedPopulation.add(individual);
-    }
-    i++;
-    Collections.sort(sortedPopulation);
+    List<Individual> parents = new ArrayList<>();
+    List<Individual> children = new ArrayList<>();
 
-    sortedPopulation.forEach(i -> System.out.println("Cost is " + i.getCost(problem)));
-    
-    // Add the first survivalRate portion of
-    List<Individual> out = new ArrayList<>();
-    for (int i=0; i < sortedPopulation.size()/2 * survivalRate; i++) {
-      out.add(sortedPopulation.get(i));
+    // Really hope here that the population is ordered by insertion order
+    int count = 0;
+    for (Individual individual : population.getPopulation()) {
+      ((count < (population.getSize() / 2)) ? parents : children).add(individual);
+      count++;
     }
-    return new Population(population);
+    Collections.sort(parents); // lowest cost first
+
+    parents.forEach(i -> System.out.println("Cost is " + i.getCost(problem)));
+
+    // Add the first survivalRate portion of
+    Set<Individual> elite = new LinkedHashSet<>();
+    Set<Individual> notElite = new LinkedHashSet<>();
+    count = 0;
+    for (Individual parent : parents) {
+      ((count < (parents.size() * survivalRate)) ? elite : notElite).add(parent);
+    }
+
+    // TournamentSelect the remaining population
+    Population notElitePop =
+        new TournamentSelection().selectSurvivors(new Population(notElite), problem, rand);
+
+    // Merge the elite and selected population
+    elite.addAll(notElitePop.getPopulation());
+    return new Population(elite);
   }
 
 }
