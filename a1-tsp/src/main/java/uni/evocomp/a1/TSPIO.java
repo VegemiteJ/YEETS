fb8f825b8e52e8c3ca3277695d81317ef387b41b
Jack Baxter <baxter.jack@outlook.com>
2018-08-14 12:05:07 +0930

    Remove debug TSPIO

--- a1-tsp/src/main/java/uni/evocomp/a1/TSPIO.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/TSPIO.java
@@ -129,7 +129,6 @@ public class TSPIO {
         List<Double> temp = new ArrayList<>();
         for (int j = 1; j <= dimension; j++) {
           Double distance = Util.euclideanDistance2D(points.get(i), points.get(j));
-          System.out.println("Point " + i + ", " + j + " = " + distance);
           temp.add(distance);
         }
         weights.add(temp);
