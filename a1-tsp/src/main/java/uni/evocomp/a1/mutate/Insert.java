546fb5e9e290f24561b8adbd6ee27a048c79e5d6
Namdrib <dphosavanhau+github@gmail.com>
2018-08-16 23:55:42 +0930

    #109 fix warnings in mutate package, re-order some mutate impls for consistency, clean up JavaDocs

--- a1-tsp/src/main/java/uni/evocomp/a1/mutate/Insert.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/mutate/Insert.java
@@ -1,9 +1,8 @@
 package uni.evocomp.a1.mutate;
 
-import java.util.Iterator;
-import java.util.List;
 import uni.evocomp.a1.Individual;
 import uni.evocomp.a1.TSPProblem;
+import uni.evocomp.util.Bounds;
 import uni.evocomp.util.IntegerPair;
 
 /**
@@ -18,21 +17,24 @@ public class Insert extends MutateImpl {
     ;
   }
 
+  @Override
+  public void run(TSPProblem problem, Individual individual, IntegerPair pair) {
+    insert(individual, pair.first, pair.second);
+  }
+
   /**
    * Move the second to follow the first, shifting the rest along to accommodate. As the indices n
    * and m are not necessarily ordered, must extract the highest and lowest
    *
-   * @param individual Individual on which to perform a mutation operation
+   * @param individual <code>Individual</code> on which to perform a mutation operation
    * @param n first index to insert
    * @param m last index to insert
    */
   private void insert(Individual individual, int n, int m)
       throws IndexOutOfBoundsException, NullPointerException {
     // Out of bounds
-    if (n < 0
-        || m < 0
-        || n >= individual.getGenotype().size()
-        || m >= individual.getGenotype().size()) {
+    if (!Bounds.inBounds(individual.getGenotype(), n)
+        || !Bounds.inBounds(individual.getGenotype(), m)) {
       throw new IndexOutOfBoundsException();
     }
     int first = Math.min(n, m);
@@ -40,10 +42,5 @@ public class Insert extends MutateImpl {
 
     individual.getGenotype().add(first + 1, individual.getGenotype().get(second));
     individual.getGenotype().remove(second + 1);
-  }
-
-  @Override
-  public void run(TSPProblem problem, Individual individual, IntegerPair pair) {
-    insert(individual, pair.first, pair.second);
   }
 }
--- a1-tsp/src/main/java/uni/evocomp/a1/mutate/Invert.java
+++ a1-tsp/src/main/java/uni/evocomp/a1/mutate/Invert.java
@@ -1,8 +1,6 @@
 package uni.evocomp.a1.mutate;
 
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
 import uni.evocomp.a1.Individual;
 import uni.evocomp.a1.TSPProblem;
 import uni.evocomp.util.IntegerPair;
@@ -18,34 +16,16 @@ public class Invert extends MutateImpl {
     ;
   }
 
-  private double calculateDifferentialCost(
-      TSPProblem problem, Individual individual, int n, int m) {
-
-    int size = problem.getSize();
-    double differentialCost = 0.0;
-
-    int a = Math.min(n, m);
-    m = Math.max(n, m);
-    n = a;
-
-    int idxI = individual.getGenotype().get(n) - 1;
-    int idxJ = individual.getGenotype().get(m) - 1;
-
-    // (i-1,i)
-    int idxINegOne = individual.getGenotype().get((n - 1 + size) % size) - 1;
-    differentialCost += problem.getWeights().get(idxINegOne, idxI);
-
-    // (j,j+1)
-    int idxJPosOne = individual.getGenotype().get((m + 1 + size) % size) - 1;
-    differentialCost += problem.getWeights().get(idxJ, idxJPosOne);
-
-    return differentialCost;
+  @Override
+  public void run(TSPProblem problem, Individual individual, IntegerPair pair) {
+    invert(problem, individual, pair.first, pair.second);
   }
 
   /**
    * Pick two alleles at random and then invert the substring between them.
    *
-   * @param individual Individual on which to perform a mutation operation
+   * @param problem <code>TSPProblem</code> used to update costs
+   * @param individual <code>Individual</code> on which to perform a mutation operation
    * @param n first index to insert
    * @param m last index to insert
    */
@@ -63,8 +43,27 @@ public class Invert extends MutateImpl {
     individual.setCost(cost);
   }
 
-  @Override
-  public void run(TSPProblem problem, Individual individual, IntegerPair pair) {
-    invert(problem, individual, pair.first, pair.second);
+  private double calculateDifferentialCost(
+      TSPProblem problem, Individual individual, int n, int m) {
+
+    int size = problem.getSize();
+    double differentialCost = 0.0;
+
+    int a = Math.min(n, m);
+    m = Math.max(n, m);
+    n = a;
+
+    int idxI = individual.getGenotype().get(n) - 1;
+    int idxJ = individual.getGenotype().get(m) - 1;
+
+    // (i - 1, i)
+    int idxINegOne = individual.getGenotype().get((n - 1 + size) % size) - 1;
+    differentialCost += problem.getWeights().get(idxINegOne, idxI);
+
+    // (j, j + 1)
+    int idxJPosOne = individual.getGenotype().get((m + 1 + size) % size) - 1;
+    differentialCost += problem.getWeights().get(idxJ, idxJPosOne);
+
+    return differentialCost;
   }
 }
