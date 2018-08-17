cf6df7550e981aa3272e3e249b943fc0d779de35
Namdrib <dphosavanhau+github@gmail.com>
2018-08-18 00:14:37 +0930

    #112 add generation script

---
+++ a1-tsp/ea_benchmark_generation.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Generate a folder with the following:
+# folder/
+# |
+# +-Main.jar
+# |
+# +-subfolder1/
+# | |
+# | +-config.properties
+# | |
+# | +- symlink_to_tests/
+# |
+# +-subfolder2/
+# | |
+# | +-config.properties
+# | |
+# | +- symlink_to_tests/
+# |
+# +-tests/
+#   |
+#   +- *.tsp
+#   |
+#   +- *.opt.tour
+# but have a subfolder for every single combination of algorithm
+
+# Everything but selectparents
+# for mutate in {In{s,v}ert,Swap} ;do
+# 	for recombine in {{Cycle,Order}Crossover} ;do
+# 		for selectparents in {UniformRandom} ;do
+# 			for selectsurvivors in {Elitism,FitnessProportional,TournamentSelect} ;do
+# 				printf "got %s %s %s %s %s\n" "$mutate" "$recombine" "$selectparents" "$selectsurvivors"
+# 			done
+# 		done
+# 	done
+# done
+
+target=ea_benchmarks
+# mkdir "$target" || exit 1
+# pushd "$target"
+
+mvn jar:jar
+
+package_path=uni.evocomp.ai
+
+alg_num=1
+for recombine in {{Order,Cycle}Crossover,EdgeRecombination,PMX} ;do
+
+	for sselection in {Elitism,Tournament,FitnessProportional} ;do
+
+		# echo "$alg_num"
+
+		target_dir="$target/$recombine-Invert-$sselection"
+
+		# Make directory
+		# echo "$recombine-Invert-$sselection"
+		mkdir -p "$target_dir"
+
+		# file for each population size
+		for pop_size in {2,5,{1,2}0}0 ;do
+			# create config.properties inside
+			echo -e "Algorithm=EA$alg_num\nSelectParents=$package_path.selectparents.UniformRandom\nRecombine=$package_path.recombine.$recombine\nMutate=$package_path.mutate.Invert\nSelectSurvivors=$package_path.selectsurvivors.$sselection\nPopulationSize=$pop_size\n" > "$target_dir/alg_$alg_num_$pop_size.properties"
+		done
+		alg_num=$((alg_num+1))
+
+		if [ ! -L "$target_dir/a.jar" ] ;then
+			ln -s "target/a1-tsp.jar" "$target_dir/a.jar"
+		fi	
+
+		# symlink to the tests
+		for item in tests/* ;do
+			if [ -f "$item" ] ;then
+				if [ ! -d "$target_dir/tests" ] ;then
+					mkdir -p "$target_dir/tests"
+				fi
+				if [ ! -L "$target_dir/$item" ] ;then
+					ln -s "$item" "$target_dir/$item"
+				fi
+			fi
+		done
+	done
+done
+
+# popd
