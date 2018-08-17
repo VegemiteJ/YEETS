fe681bcc2d6ecdad8557d44cd32b758518476235
Jack Baxter <Baxter.Jack@outlook.com>
2018-08-17 14:32:36 +0930

    added phoenix script

---
+++ a1-tsp/src/main/phoenix/LocalSearchBenchmark.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -p skylake                                                # partition (this is the queue your job will be added to)
+#SBATCH -n 4                                                    # number of cores (sequential job uses 1 core)
+#SBATCH --time=12:00:00                                         # time allocation, which has the format (D-HH:MM:SS), here set to 1 hour
+#SBATCH --mem=64GB                                               # specify memory required per node (here set to 4 GB)
+
+# Notification configuration
+#SBATCH --mail-type=ALL
+#SBATCH --mail-type=TIME_LIMIT_90
+#SBATCH --mail-user=a1666857@student.adelaide.edu.au            # Email to which notification will be sent
+
+# Name
+#SBATCH -J LocalSearchBenchmark
+DEPLOY_DIR="/fast/users/a1666857/Projects/EvoComp_Deployment/LocalSearchBenchmark"
+
+module load Java/1.8.0_121
+java -jar a1-tsp-0.1.jar
