4fd63e1f5453d8247702ee1bb66e28956ee42ab3
Namdrib <dphosavanhau+github@gmail.com>
2018-08-19 20:54:27 +0930

    #85 completely remove Evaluate

--- a1-tsp/src/main/java/uni/evocomp/a1/evaluate/Evaluate.java
+++
@@ -1,8 +0,0 @@
-package uni.evocomp.a1.evaluate;
-
-import uni.evocomp.a1.Individual;
-import uni.evocomp.a1.TSPProblem;
-
-public interface Evaluate {
-  public double evaluate(TSPProblem problem, Individual individual);
-}
